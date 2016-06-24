7cfece1529e76e46ce2b85c8c8b239f2f6abe6a9
godbasin <wangbeishan@163.com>
2016-06-24 15:48:38 +0800

    20160624

---
+++ angular-notes/1-create-angular-project/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('angularApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
