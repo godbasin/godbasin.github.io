51640ba39d7d6ef1335cd3ad47dd116527b46ce9
godbasin <wangbeishan@163.com>
2016-06-27 16:30:01 +0800

    20160627

---
+++ angular-notes/4-create-header/scripts/app.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var app = angular.module('angularTestApp', [
+	'ngCookies',
+	'ngResource',
+	'ngSanitize',
+	'ngRoute',
+], function($httpProvider) {
+	// Use x-www-form-urlencoded Content-Type
+	$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+	var param = function(obj) {
+		var query = '',
+			name, value, fullSubName, subName, subValue, innerObj, i;
+		for (name in obj) {
+			value = obj[name];
+
+			if (value instanceof Array) {
+				for (i = 0; i < value.length; ++i) {
+					subValue = value[i];
+					/*fullSubName = name + '[' + i + ']';*/
+					fullSubName = name;
+					innerObj = {};
+					innerObj[fullSubName] = subValue;
+					query += param(innerObj) + '&';
+				}
+			} else if (value instanceof Object) {
+				for (subName in value) {
+					subValue = value[subName];
+					fullSubName = name /* + '[' + subName + ']'*/ ;
+					innerObj = {};
+					innerObj[fullSubName] = subValue;
+					query += param(innerObj) + '&';
+				}
+			} else if (value !== undefined && value !== null)
+				query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+		}
+		return query.length ? query.substr(0, query.length - 1) : query;
+	};
+	// Override $http service's default transformRequest
+	$httpProvider.defaults.transformRequest = [function(data) {
+		return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+	}];
+});
+app.config(function($routeProvider) {
+	$routeProvider
+	//login路由
+		.when('/login', {
+			templateUrl: 'views/login.html', //login的html页面
+			controller: 'LoginCtrl' //login的控制器
+		})
+		.when('/index', {
+			templateUrl: 'views/index.html', //index的html页面
+			controller: '' //index的控制器
+		})
+		//页面重定向
+		.otherwise({
+			redirectTo: '/login'
+		});
+});
---
+++ angular-notes/4-create-header/scripts/controllers/loginCtrl.js
@@ -0,0 +1,28 @@
+'use strict';
+
+app.controller('LoginCtrl', ['$scope', '$http', function($scope, $http) {
+	//定义submit事件，对应html中ng-submit
+	$scope.submit = function() {
+		window.location.href = 'index.html#/index';
+		//http服务POST账户密码
+		/*$http({
+				method: 'POST',
+				url: 'your url',
+				params: {
+					"username": $scope.username, //ng-model双向绑定的用户名
+					"password": $scope.password //ng-model双向绑定的密码
+				}, 
+			})
+			.success(function(data) {				
+				if (data.result === 'success') {			
+					window.location.href = 'index.html#/index'; //判断登录成功，跳转
+				} else {					
+					alert("error"); //登录失败提示
+				}				
+			})
+			//连接服务失败
+			.error(function() {
+				alert("connecting fail");
+			});*/
+	};
+}]);
