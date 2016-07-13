aef5e3c2933a3966254bf4751366451812474dec
wangbeishan <wangbeishan@sensetime.com>
2016-07-13 16:39:38 +0800

    2016.07.13

---
+++ vue-notes/3-create-header/src/components/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div is="my-header" current="index"></div><!--使用is绑定组件，current传入prop数据-->
+  <div class="container">Hello Vue!</div>
+</template>
+<script>
+// 导入Header组件
+import MyHeader from './Header.vue'
+export default {
+  components: {
+    MyHeader
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
