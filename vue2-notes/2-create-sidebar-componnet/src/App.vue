02637db41d817ed5202f1ed8489128bf9d0c810d
wangbeishan <wangbeishan@163.com>
2016-11-05 13:53:04 +0800

    updated

---
+++ vue2-notes/2-create-sidebar-componnet/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<div id="app">
+		<div class="container body">
+			<div class="main_container">
+                <!--使用Sidebar组件-->
+				<Sidebar></Sidebar>
+                <!-- page content -->
+				<div class="right_col" role="main"></div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+    import Sidebar from './components/Sidebar'
+
+
+    export default {
+        name: 'App',
+        components: {
+            Sidebar
+        },
+        created() {
+            $('body').attr('class', 'nav-md');
+        },
+        mounted() {
+            $('.right_col').css('min-height', $(window).height());
+        }
+    }
+</script>
+
+<style>
+
+</style>
